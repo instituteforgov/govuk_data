204af40b1eecb66c5b060bc38696d0d1c2ee4f8b
philipnye <philipnye@users.noreply.github.com>
2024-05-03 20:22:49 +0100

    Ingest reviewed matches and save to SQL

--- govuk_data/people/match_govuk_people.py
+++ govuk_data/people/match_govuk_people.py
@@ -23,6 +23,8 @@ import os
 
 import pandas as pd
 import pandas.io.formats.excel
+import sqlalchemy
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 
 from ds_utils import database_operations as dbo
 from ds_utils import string_operations as so
@@ -117,3 +119,67 @@ df_match[[
 ]].to_excel(
     'data/match_20240503.xlsx'
 )
+
+# %%
+# INGEST REVIEWED MATCHES
+df_reviewed = pd.read_excel(
+    'data/match_20240503.xlsx',
+    index_col=None
+)
+
+# %%
+# EDIT REVIEWED MATCHES
+# This picks accepted fuzzy matches and supplied manual matches
+df_reviewed = pd.concat(
+    [
+        df_reviewed[
+            df_reviewed['Accept'] == 'Y'
+        ][[
+            'df_left_id',
+            'df_right_id',
+            'name_df_left',
+            'name_df_right',
+            'govuk_string'
+        ]],
+        df_reviewed[
+            (df_reviewed['Accept'] == 'N') &
+            (df_reviewed['Replacement name'].notna())
+        ][[
+            'df_left_id',
+            'df_right_id',
+            'name_df_left',
+            'Replacement name',
+            'Replacement govuk_string'
+        ]].rename(
+            columns={
+                'Replacement name': 'name_df_right',
+                'Replacement govuk_string': 'govuk_string'
+            }
+        )
+    ]
+)
+
+# %%
+# SAVE TO
+df_reviewed[[
+    'df_left_id',
+    'name_df_left',
+    'govuk_string',
+]].rename(
+    columns={
+        'df_left_id': 'id',
+        'name_df_left': 'name'
+    }
+).to_sql(
+    'ukgovt.minister_ids_govuk_strings_20240503',
+    schema='analysis',
+    con=connection,
+    dtype={
+        'id': UNIQUEIDENTIFIER,
+        'name': sqlalchemy.types.NVARCHAR(length=255),
+        'govuk_string': sqlalchemy.types.NVARCHAR(length=255)
+    },
+    index=False,
+)
+
+# %%
