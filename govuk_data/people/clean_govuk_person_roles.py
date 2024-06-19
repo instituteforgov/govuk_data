059932a2040f8e39857c910ceed95009d1b11632
philipnye <philipnye@users.noreply.github.com>
2024-06-19 13:53:49 +0100

    - Apply replace_ampersand() - Apply identify_ministers_on_leave_acting() - Apply remove_joint_post_name() - Apply handle_equalities_minister_post_name() - Apply handle_parliamentary_secretary_post_name() - Apply remove_lords_minister_post_names()

--- govuk_data/people/clean_govuk_person_roles.py
+++ govuk_data/people/clean_govuk_person_roles.py
@@ -4,7 +4,7 @@
 
 '''
     Purpose
-        Read in gov.uk person page data from API endpoint
+        Clean GOV.UK person page data
     Inputs
         - SQL: analysis.ukgovt.minister_govuk_people_page_content_20240503
     Outputs
@@ -21,7 +21,7 @@ import pandas as pd
 from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 
 from ds_utils import database_operations as dbo
-from utils.utils import standardise_mos_puss_post_name
+import utils.utils as utils
 
 # %%
 # CONNECT TO D/B
@@ -46,21 +46,39 @@ df = pd.read_sql_query(
 )
 
 # %%
-# CLEAN POST NAME AND ADD POST RANK
-# Ref: https://stackoverflow.com/a/66267940/4659442
-df.insert(
-    df.columns.get_loc('post_name') + 1,
-    'post_name_clean',
-    None
-)
-df.insert(
-    df.columns.get_loc('post_name') + 2,
-    'post_rank',
-    None
+# CLEAN DATA
+# Remove ampersands
+df['post_name_clean'] = df['post_name_clean'].apply(utils.replace_ampersand)
+
+# %%
+# Identify ministers on leave and doing roles in an acting capacity
+df[['post_name_clean', 'is_on_leave', 'is_acting', 'leave_reason']] = pd.DataFrame(
+    df['post_name_clean'].apply(utils.identify_ministers_on_leave_acting).to_list()
 )
 
+# %%
+# Remove joint post names
+df['post_name_clean'] = df['post_name_clean'].apply(utils.remove_joint_post_name)
+
+# %%
+# Handle post names that are denormalised by having an equalities minister name appended
+df['post_name_clean'] = df['post_name_clean'].apply(utils.handle_equalities_minister_post_name)
+
+# %%
+# Handle Parliamentary Secretary post names
 df[['post_name_clean', 'post_rank']] = pd.DataFrame(
-    df['post_name'].apply(standardise_mos_puss_post_name).to_list()
+    df['post_name_clean'].apply(utils.handle_parliamentary_secretary_post_name).to_list()
+)
+
+# %%
+# Remove details of Lords minister roles
+df['post_name_clean'] = df['post_name_clean'].apply(utils.remove_lords_minister_post_names)
+
+# %%
+# Standardise MoS, PUSS post name
+# Ref: https://stackoverflow.com/a/66267940/4659442
+df[['post_name_clean', 'post_rank']] = pd.DataFrame(
+    df['post_name_clean'].apply(utils.standardise_mos_puss_post_name).to_list()
 )
 
 # %%
