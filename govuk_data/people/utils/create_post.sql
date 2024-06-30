23c526523bb69e7825ec323171bebdcfd27c832f
philipnye <philipnye@users.noreply.github.com>
2024-06-30 14:47:19 +0100

    Add organisation_start_date, organisation_end_date

--- govuk_data/people/utils/create_post.sql
+++ govuk_data/people/utils/create_post.sql
@@ -7,4 +7,6 @@ select
 from core.organisation o
 where
     o.name = '{organisation_name}' and
-    o.short_name = '{organisation_short_name}'
+    o.short_name = '{organisation_short_name}' and
+    o.start_date = '{organisation_start_date}' and
+    o.end_date = '{organisation_end_date}'
