d6c4ed8f61d622688750d92e0033fd05bf17ce54
philipnye <philipnye@users.noreply.github.com>
2024-06-30 14:46:37 +0100

    Add organisation_start_date, organisation_end_date

--- govuk_data/people/utils/identify_posts_to_add.sql
+++ govuk_data/people/utils/identify_posts_to_add.sql
@@ -4,6 +4,8 @@
 -- of an appointment
 -- NB: This excludes appointments in the d/b matched to more than a single appointment in the
 -- GOV.UK data - we handle these cases manually
+-- NB: This adds on organisation start and end dates so we can distinguish between organisations where
+-- the same name has been used more than once (e.g. Department for Culture, Media and Sport, Department for Education)
 select distinct
     w1.post_name_govuk post_name,
     w1.organisation_name_ifg organisation_name,
@@ -11,8 +13,14 @@ select distinct
     case
         when w1.post_rank_govuk is null then w1.post_rank_ifg
         else w1.post_rank_govuk
-    end post_rank
+    end post_rank,
+    o.start_date organisation_start_date,
+    o.end_date organisation_end_date
 from workflow.[50556707-3276-404a-8a3f-cee24d329bae] w1
+    left join core.organisation o on
+        w1.organisation_name_ifg = o.name and
+        w1.start_date_ifg >= isnull(o.start_date, '1900-01-01') and
+        w1.end_date_ifg <= isnull(o.end_date, '9999-12-31')
 where
     w1.reviewed = 1 and
     w1.match_accepted = 1 and
